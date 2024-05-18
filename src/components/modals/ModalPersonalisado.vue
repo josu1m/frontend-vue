485589aeee03911a245b36c33e2fa8f5becb0815
mendozam6991m <mendozam6991m@gmail.com>
2024-05-18 20:07:48 +0000

    Cuarto subida

--- src/components/modals/ModalPersonalisado.vue
+++ src/components/modals/ModalPersonalisado.vue
@@ -104,4 +104,31 @@
     }
   }
   </style>
+
+  <!--<template>
+    <button @click="openModal">Abrir Modal</button>
+    <Modal :show="showModal" @close="closeModal">
+      <template #header>
+        <h2>Título 11</h2>
+      </template>
+      <p>Este es el contenido del modal JUAN</p>
+      <button @click="closeModal">Cerrar</button>
+    </Modal>
+  </template>
+  
+  <script setup>
+  import Modal from "@/components/modals/ModalPersonalisado.vue";
+  import { ref } from "vue";
+  
+  const showModal = ref(false);
+  
+  const openModal = () => {
+    showModal.value = true;
+  };
+  
+  const closeModal = () => {
+    showModal.value = false;
+  };
+  </script>
+  -->
   
--- src/views/PruebaView.vue
+++ src/views/PruebaView.vue
@@ -1,14 +1,64 @@
 <template>
-  <button @click="showModal = true">Abrir Modal</button>
-  <Modal :isVisible="showModal" @close="showModal = false">
-    <h2>Contenido del Modal</h2>
-    <p>Este es un ejemplo de contenido dentro del modal</p>
-  </Modal>
+  <div>
+    <button @click="showModal = true">Abrir Modal</button>
+
+    <transition name="modal-fade">
+      <div v-if="showModal" class="modal">
+        <div class="modal-content">
+          <span class="close" @click="showModal = false">&times;</span>
+          <p>¡Hola! Este es un modal simple.</p>
+        </div>
+      </div>
+    </transition>
+  </div>
 </template>
 
-<script setup>
-import Modal from '@/components/modals/Modal.vue';
+<script setup lang="ts">
 import { ref } from 'vue';
 
 const showModal = ref(false);
 </script>
+
+<style scoped>
+.modal {
+  display: block;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.modal-fade-enter-active, .modal-fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.modal-fade-enter, .modal-fade-leave-to {
+  opacity: 0;
+}
+</style>
